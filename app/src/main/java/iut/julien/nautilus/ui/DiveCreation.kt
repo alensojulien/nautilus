69af5bad62bdd0573b146e2f392f6a62cb3498f3
Alenso Lopes <alenso.lopes@gmail.com>
2024-03-23 18:21:14 +0100

    feat: api-request for role

--- app/src/main/java/iut/julien/nautilus/ui/DiveCreation.kt
+++ app/src/main/java/iut/julien/nautilus/ui/DiveCreation.kt
@@ -28,6 +28,15 @@ class DiveCreation {
         val levelList = remember {
             mutableStateListOf("")
         }
+        val director = remember {
+            mutableStateListOf("")
+        }
+        val security = remember {
+            mutableStateListOf("")
+        }
+        val pilote = remember {
+            mutableStateListOf("")
+        }
         LaunchedEffect(Unit) {
             val list = requestToAPIData(URL("https://dev-sae301grp3.users.info.unicaen.fr/api/divinglocation"), "DL_NAME")
             locationList.addAll(list)
@@ -35,6 +44,12 @@ class DiveCreation {
             boatList.addAll(listBoat)
             val listLevel = requestToAPIData(URL("https://dev-sae301grp3.users.info.unicaen.fr/api/prerogative"), "PRE_CODE")
             levelList.addAll(listLevel)
+            val listDirector = getUserRole("DIRECTOR")
+            director.addAll(listDirector)
+            val listPilote = getUserRole("PILOTE")
+            pilote.addAll(listPilote)
+            val listSecurity = getUserRole("SECURITY")
+            security.addAll(listSecurity)
         }
         AndroidView(
             factory = { context ->
@@ -42,9 +57,15 @@ class DiveCreation {
                 val locationSpinner = view.findViewById<Spinner>(R.id.DS_LOCATION)
                 val boatSpinner = view.findViewById<Spinner>(R.id.DS_BOAT)
                 val levelSpinner = view.findViewById<Spinner>(R.id.DS_LEVEL)
+                val directorSpinner = view.findViewById<Spinner>(R.id.DS_DIRECTOR)
+                val piloteSpinner = view.findViewById<Spinner>(R.id.DS_PILOT)
+                val securitySpinner = view.findViewById<Spinner>(R.id.DS_SECURITY)
                 fillList(locationSpinner, locationList)
                 fillList(boatSpinner, boatList)
                 fillList(levelSpinner, levelList)
+                fillList(directorSpinner, director)
+                fillList(piloteSpinner, pilote)
+                fillList(securitySpinner, security)
                 view
             }
         )
@@ -66,7 +87,6 @@ class DiveCreation {
                     return@withContext location
                 }
             }
-
         }
     }
 
@@ -88,5 +108,86 @@ class DiveCreation {
         val adapter = ArrayAdapter(spinner.context, android.R.layout.simple_spinner_item, list)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinner.adapter = adapter
+    }
+
+    private suspend fun getUserRole(roles: String): MutableList<String> {
+        return withContext(Dispatchers.IO){
+            val url = URL("https://dev-sae301grp3.users.info.unicaen.fr/api/user")
+            val response = StringBuffer()
+            with(url.openConnection() as HttpsURLConnection) {
+                requestMethod = "GET"
+                BufferedReader(inputStream.reader()).use {
+                    var inputLine = it.readLine()
+                    while (inputLine != null) {
+                        response.append(inputLine)
+                        inputLine = it.readLine()
+                    }
+                    it.close()
+                }
+            }
+            val role = getRoleAttribution(roles)
+            val user = mutableListOf<String>()
+            var json = JSONObject()
+            try{
+                json = JSONObject(response.toString())
+            }catch (e: Exception){
+                e.printStackTrace()
+            }
+
+            for(i in 0 until json.getJSONArray("data").length()){
+                val id = json.getJSONArray("data").getJSONObject(i).getString("US_ID")
+                if(role.contains(id)){
+                    user.add(json.getJSONArray("data").getJSONObject(i).getString("US_NAME"))
+                }
+            }
+            return@withContext user
+        }
+    }
+
+    private suspend fun getRoleAttribution(roles: String): MutableList<String> {
+       return withContext(Dispatchers.IO){
+            val dataList = mutableListOf<String>()
+            val url = URL("https://dev-sae301grp3.users.info.unicaen.fr/api/roleattribution")
+            val response = StringBuffer()
+            with(url.openConnection() as HttpsURLConnection) {
+                requestMethod = "GET"
+                BufferedReader(inputStream.reader()).use {
+                    var inputLine = it.readLine()
+                    while (inputLine != null) {
+                        response.append(inputLine)
+                        inputLine = it.readLine()
+                    }
+                    it.close()
+                }
+            }
+            var json = JSONObject()
+            try{
+                json = JSONObject(response.toString())
+            }catch (e: Exception){
+                e.printStackTrace()
+            }
+            for(i in 0 until json.getJSONArray("data").length()){
+                val role = json.getJSONArray("data").getJSONObject(i).getString("ROL_CODE")
+                val id = json.getJSONArray("data").getJSONObject(i).getString("US_ID")
+                when(roles){
+                    "DIRECTOR" -> {
+                        if(role == "DIR"){
+                            dataList.add(id)
+                        }
+                    }
+                    "PILOTE" -> {
+                        if(role == "PIL"){
+                            dataList.add(id)
+                        }
+                    }
+                    "SECURITY" -> {
+                        if(role == "SEC"){
+                            dataList.add(id)
+                        }
+                    }
+                }
+            }
+            return@withContext dataList
+        }
     }
 }
