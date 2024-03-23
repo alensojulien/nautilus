da5769d7d7489504b04fe9fbd770ccdf2ff3449c
Alenso Lopes <alenso.lopes@gmail.com>
2024-03-23 17:04:05 +0100

    feat: createDive view

--- app/src/main/java/iut/julien/nautilus/ui/DiveCreation.kt
+++ app/src/main/java/iut/julien/nautilus/ui/DiveCreation.kt
@@ -1,11 +1,26 @@
 package iut.julien.nautilus.ui
 
+import android.view.LayoutInflater
+import android.widget.Spinner
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.viewinterop.AndroidView
+import iut.julien.nautilus.R
 
 class DiveCreation {
     @Composable
     fun DiveCreationScreen() {
-        Text("DiveCreation")
+        AndroidView(
+            factory = { context ->
+                val view = LayoutInflater.from(context).inflate(R.layout.activity_main, null)
+                val locationSpinner = view.findViewById<Spinner>(R.id.DS_LOCATION)
+
+                view
+            }
+        )
+    }
+
+    fun requestToAccessLocationList(){
+        
     }
 }
